f975d4a683bf7b939841fe8d573a9c1701e90231
ajovicic0321 <aleks.jovicic0321@gmail.com>
2025-07-12 01:27:39 +0300

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  };
+  app.useStaticAssets(join(__dirname, '..', 'assets'), {
+    prefix: '/assets/',
+  });
+  app.enableCors(corsOptions);
 
-  const origins = process.env.ORIGIN ? process.env.ORIGIN.split(',').map(o => o.trim()) : ['http://localhost:3000'];
   const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3003;
 
-  app.enableCors({
-    origin: origins,
-    credentials: true,
-  });
   await app.listen(port);
 }
 bootstrap();
